3df615e7efa5762608af43bd5f4a452d567db21e
Gabriel Koller <gabriel.koller07@gmail.com>
2023-10-11 15:25:58 -0300

    aula 126 salvando novo commit

---
+++ src/application/Program21.java
@@ -0,0 +1,23 @@
+package application;
+
+import java.util.Scanner;
+
+public class Program21 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Digite a velocidade: ");
+		
+		int velocity = sc.nextInt();
+		
+		System.out.printf("A velocidade convertida é %.0f km/h", ms(velocity));
+
+	}
+	
+	public static double ms(int velocity) {
+		return velocity * 3.6;
+	}
+
+}
